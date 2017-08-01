0903709668fe99778f422300c6d5591cbfa3c24d
Rebecca Cremona <rcremona@law.harvard.edu>
2017-08-01 10:40:38 -0400

    Better data validation and error handling.

--- perma-payments/perma_payments/views.py
+++ perma-payments/perma_payments/views.py
@@ -3,12 +3,20 @@ import hmac
 import base64
 
 from django.conf import settings
+from django.core.exceptions import ValidationError
+from django.db import transaction
+from django.http.response import HttpResponseBadRequest
 from django.shortcuts import render
+from django.views.decorators.debug import sensitive_post_parameters
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
 
 from .constants import *
 from .models import *
+
+import logging
+logger = logging.getLogger(__name__)
+
 
 def index(request):
     return render(request, 'generic.html', {'heading': "perma-payments",
@@ -16,32 +24,41 @@ def index(request):
 
 @csrf_exempt
 @require_http_methods(["POST"])
+@sensitive_post_parameters('signature')
 def subscribe(request):
     """
     Processes user-initiated subscription requests from Perma.cc;
     Redirects user to CyberSource for payment.
     """
     try:
-        data = request.POST.dict()
-    except:
-        # if something goes wrong, we should log it, and display an error page
-        raise
+        data = {
+            'registrar': request.POST.__getitem__('registrar'),
+            'amount': request.POST.__getitem__('amount'),
+            'recurring_amount': request.POST.__getitem__('recurring_amount'),
+            'recurring_frequency': request.POST.__getitem__('recurring_frequency')
+        }
+    except KeyError as e:
+        logger.warning('Incomplete POST from Perma.cc subscribe form: missing {}'.format(e))
+        return HttpResponseBadRequest()
     try:
-        s_agreement = SubscriptionAgreement(
-            registrar=data['registrar'],
-            status='Pending'
-        )
-        s_agreement.save()
-        s_request = SubscriptionRequest(
-            subscription_agreement=s_agreement,
-            amount=data['amount'],
-            recurring_amount=data['recurring_amount'],
-            recurring_frequency=data['recurring_frequency']
-        )
-        s_request.save()
-    except:
-        # if something goes wrong, we should log it, and display an error page
-        raise
+        with transaction.atomic():
+            s_agreement = SubscriptionAgreement(
+                registrar=data['registrar'],
+                status='Pending'
+            )
+            s_agreement.full_clean()
+            s_agreement.save()
+            s_request = SubscriptionRequest(
+                subscription_agreement=s_agreement,
+                amount=data['amount'],
+                recurring_amount=data['recurring_amount'],
+                recurring_frequency=data['recurring_frequency']
+            )
+            s_request.full_clean()
+            s_request.save()
+    except ValidationError as e:
+        logger.warning('Invalid POST from Perma.cc subscribe form: {}'.format(e))
+        return HttpResponseBadRequest()
 
     signed_fields = {
         'access_key': settings.CS_ACCESS_KEY,
