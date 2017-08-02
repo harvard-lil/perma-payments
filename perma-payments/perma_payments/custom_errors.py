27a82ad9d2b84bee4fdd0a23d408ad9827a7d55a
Rebecca Cremona <rcremona@law.harvard.edu>
2017-08-02 13:02:04 -0400

    Import Werkzeug error message instead of hard coding

--- perma-payments/perma_payments/custom_errors.py
+++ perma-payments/perma_payments/custom_errors.py
@@ -1,9 +1,11 @@
+from werkzeug.exceptions import BadRequest
+
 from django.shortcuts import render
 
 
 def bad_request(request):
     context = {
         'heading': "400 Bad Request",
-        'message': "The browser (or proxy) sent a request that this server could not understand."
+        'message': BadRequest.description
     }
     return render(request, 'generic.html', context, status=400)
