53a1a669d78daaec50e651caf4d6e2c7276193ee
Rebecca Cremona <rcremona@law.harvard.edu>
2017-08-08 17:00:50 -0400

    Use public key encryption for responses from CyberSource instead of symmetric encryption.

--- perma-payments/perma_payments/security.py
+++ perma-payments/perma_payments/security.py
@@ -1,7 +1,7 @@
 import hashlib
 import hmac
 import base64
-import nacl.secret
+from nacl.public import Box, PrivateKey, PublicKey
 
 from django.conf import settings
 
@@ -19,9 +19,42 @@ def sign_data(data_string):
     hash = hmac.new(secret, message, hashlib.sha256)
     return base64.b64encode(hash.digest())
 
+
+def generate_public_private_keys():
+    secret_a = PrivateKey.generate()
+    public_a = secret_a.public_key
+    secret_b = PrivateKey.generate()
+    public_b = secret_b.public_key
+    return {
+        'a': {
+            'secret': secret_a._private_key,
+            'public': public_a._public_key,
+        },
+        'b': {
+            'secret': secret_b._private_key,
+            'public': public_b._public_key,
+        }
+    }
+
+
 def encrypt_for_storage(message, nonce):
     """
-    Basic secret key encryption ala pynacl.
+    Basic public key encryption ala pynacl.
     """
-    box = nacl.secret.SecretBox(settings.STORAGE_SECRET_KEY['key'])
+    box = Box(PrivateKey(settings.STORAGE_SECRET_KEY['app_secret_key']), PublicKey(settings.STORAGE_SECRET_KEY['vault_public_key']))
     return box.encrypt(message, nonce)
+
+
+def decrypt_from_storage(ciphertext):
+    """
+    Decrypt bytes encrypted via encrypt_for_storage
+
+    To use in real life: get the vault secret key, and add to settings.py:
+    python manage.py shell
+    >>> from perma_payments.models import *
+    >>> from perma_payments.security import *
+    >>> resp = SubscriptionRequestResponse.objects.get(pk=????????)
+    >>> decrypt_full_response(bytes(resp.full_response))
+    """
+    box = Box(PrivateKey(settings.STORAGE_SECRET_KEY['vault_secret_key']), PublicKey(settings.STORAGE_SECRET_KEY['app_public_key']))
+    return box.decrypt(ciphertext)
--- perma-payments/perma_payments/tests.py
+++ perma-payments/perma_payments/tests.py
@@ -1,6 +1,6 @@
 # from django.test import TestCase
-from .security import data_to_string, sign_data
-
+from .security import *
+from nacl.public import PrivateKey, PublicKey
 
 def test_data_to_string_sorted():
     """
@@ -34,3 +34,20 @@ def test_sign_data():
     """
     data_string = "bar=bamph,foo=baz,signed_field_names=bar,foo,signed_field_names"
     assert sign_data(data_string) == b"wmEE0YLoDwXHf6kRe1e8AV9OcaFGNI+2qRQ8t9gS1Fk="
+
+
+def test_generate_public_private_keys():
+    keys = generate_public_private_keys()
+    expected_keys = ['a', 'b']
+    assert list(keys.keys()) == expected_keys
+    for key in expected_keys:
+        assert list(keys[key].keys()) == ['secret', 'public']
+        assert keys[key]['secret'] != keys[key]['public']
+        assert isinstance(PrivateKey(keys[key]['secret']), PrivateKey)
+        assert isinstance(PublicKey(keys[key]['public']), PublicKey)
+
+
+def test_storage_encrypt_and_decrypt():
+    message = 'hi there'
+    ci = encrypt_for_storage(bytes(message, 'utf-8'), (1).to_bytes(24, byteorder='big'))
+    assert str(decrypt_from_storage(ci), 'utf-8') == message
