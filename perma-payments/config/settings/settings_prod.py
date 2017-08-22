943a3c1bf45366fa0730e8a4384065a42ca2d421
Ben Steinberg <bsteinberg@law.harvard.edu>
2017-08-22 11:59:35 -0400

    Add empty email settings to prod config

--- perma-payments/config/settings/settings_prod.py
+++ perma-payments/config/settings/settings_prod.py
@@ -9,3 +9,10 @@ LOGGING['loggers'] = {
         'propagate': True,
     },
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST =
+EMAIL_HOST_USER =
+EMAIL_HOST_PASSWORD =
