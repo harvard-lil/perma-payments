c6071ea40e239c19bcbdce08e879540c3558ffc0
Rebecca Cremona <rcremona@law.harvard.edu>
2018-08-31 12:16:16 -0400

    Fix template syntax; update test's expectation.

--- perma-payments/perma_payments/tests/test_routes.py
+++ perma-payments/perma_payments/tests/test_routes.py
@@ -872,6 +872,7 @@ def test_cancel_request_post_subscription_happy_path(client, cancel_request, com
         'customer_type': cancel_request['valid_data']['customer_type'],
         'search_url': CS_SUBSCRIPTION_SEARCH_URL[settings.CS_MODE],
         'perma_url': settings.PERMA_URL,
+        'individual_detail_path': settings.INDIVIDUAL_DETAIL_PATH,
         'registrar_detail_path': settings.REGISTRAR_DETAIL_PATH,
         'registrar_users_path': settings.REGISTRAR_USERS_PATH,
         'merchant_reference_number': complete_standing_sa.subscription_request.reference_number
