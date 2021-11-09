70434caa736369254334601144cd7df374357bf7
Ben Steinberg <bsteinberg@law.harvard.edu>
2021-11-09 11:05:53 -0500

    Minimal upgrade to Postgres 12

--- web/config/settings/settings_base.py
+++ web/config/settings/settings_base.py
@@ -79,6 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
+        'PASSWORD': 'example',
         'HOST': 'db',
         'PORT': 5432,
     }
