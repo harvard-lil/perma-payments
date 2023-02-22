374a7d8e726aa539bf3918a0fc28e3b3049e042c
Rebecca Cremona <rcremona@law.harvard.edu>
2023-02-22 10:49:33 -0500

    Django 3.1

--- web/perma_payments/urls.py
+++ web/perma_payments/urls.py
@@ -1,18 +1,18 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^cancel-request/$', views.cancel_request, name='cancel_request'),
-    url(r'^cybersource-callback/$', views.cybersource_callback, name='cybersource_callback'),
-    url(r'^purchase/$', views.purchase, name='purchase'),
-    url(r'^acknowledge-purchase/$', views.acknowledge_purchase, name='acknowledge_purchase'),
-    url(r'^purchase-history/$', views.purchase_history, name='purchase_history'),
-    url(r'^subscribe/$', views.subscribe, name='subscribe'),
-    url(r'^subscription/$', views.subscription, name='subscription'),
-    url(r'^update-statuses/$', views.update_statuses, name='update_statuses'),
-    url(r'^update/$', views.update, name='update'),
-    url(r'^change/$', views.change, name='change'),
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^cancel-request/$', views.cancel_request, name='cancel_request'),
+    re_path(r'^cybersource-callback/$', views.cybersource_callback, name='cybersource_callback'),
+    re_path(r'^purchase/$', views.purchase, name='purchase'),
+    re_path(r'^acknowledge-purchase/$', views.acknowledge_purchase, name='acknowledge_purchase'),
+    re_path(r'^purchase-history/$', views.purchase_history, name='purchase_history'),
+    re_path(r'^subscribe/$', views.subscribe, name='subscribe'),
+    re_path(r'^subscription/$', views.subscription, name='subscription'),
+    re_path(r'^update-statuses/$', views.update_statuses, name='update_statuses'),
+    re_path(r'^update/$', views.update, name='update'),
+    re_path(r'^change/$', views.change, name='change'),
 ]
 
